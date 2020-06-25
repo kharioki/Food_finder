c5a29e834ad867252cc744df1c747e5d1fce1989
kharioki <antolisah@gmail.com>
2020-06-25 19:23:36 +0300

    using screen options

--- App.js
+++ App.js
@@ -17,10 +17,23 @@ const Home = ({navigation}) => {
   );
 };
 
-const Details = () => {
+const Details = ({navigation}) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details screen</Text>
+      <Button
+        title="Go to details screen... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button
+        title="Go to Home screen"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go to the first screen"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 };
@@ -28,8 +41,21 @@ const Details = () => {
 export default App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#009387',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Overview'}}
+        />
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
