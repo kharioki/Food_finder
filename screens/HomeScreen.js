4d1bd5d23a1da75f7667df39de273b4cf079595a
kharioki <antolisah@gmail.com>
2020-08-08 21:20:55 +0300

    recently viewed cards, and category names

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
+  ScrollView,
   View,
   TouchableOpacity,
   StatusBar,
   Image,
   StyleSheet,
+  Text,
 } from 'react-native';
 import {useTheme} from '@react-navigation/native';
 
@@ -14,12 +16,14 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+import StarRating from '../components/StarRating';
+
 export default Home = () => {
   const {colors} = useTheme();
   const theme = useTheme();
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
       <View style={styles.sliderContainer}>
         <Swiper
@@ -56,6 +60,7 @@ export default Home = () => {
           <View style={styles.categoryIcon}>
             <Ionicons name="ios-restaurant" size={35} color="#ff6347" />
           </View>
+          <Text style={styles.categoryBtnTxt}>Restaurant</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.categoryBtn} onPress={() => {}}>
           <View style={styles.categoryIcon}>
@@ -65,11 +70,13 @@ export default Home = () => {
               color="#ff6347"
             />
           </View>
+          <Text style={styles.categoryBtnTxt}>Fastfood Center</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.categoryBtn} onPress={() => {}}>
           <View style={styles.categoryIcon}>
             <MaterialCommunityIcons name="food" size={35} color="#ff6347" />
           </View>
+          <Text style={styles.categoryBtnTxt}>Snacks Corner</Text>
         </TouchableOpacity>
       </View>
 
@@ -78,19 +85,82 @@ export default Home = () => {
           <View style={styles.categoryIcon}>
             <Fontisto name="hotel" size={35} color="#ff6347" />
           </View>
+          <Text style={styles.categoryBtnTxt}>Hotels</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.categoryBtn} onPress={() => {}}>
           <View style={styles.categoryIcon}>
             <Ionicons name="md-restaurant" size={35} color="#ff6347" />
           </View>
+          <Text style={styles.categoryBtnTxt}>Dineouts</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.categoryBtn} onPress={() => {}}>
           <View style={styles.categoryIcon}>
             <MaterialIcons name="expand-more" size={35} color="#ff6347" />
           </View>
+          <Text style={styles.categoryBtnTxt}>Show More</Text>
         </TouchableOpacity>
       </View>
-    </View>
+
+      <View style={styles.cardsWrapper}>
+        <Text
+          style={{
+            alignSelf: 'center',
+            fontSize: 18,
+            fontWeight: 'bold',
+            color: colors.text,
+          }}>
+          Recently Viewed
+        </Text>
+        <View style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../assets/banners/food-banner3.jpg')}
+              resizeMode="cover"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Amazing Food Place</Text>
+            <StarRating ratings={4} reviews={99} />
+            <Text style={styles.cardDetails}>
+              Some description for this amazing food place
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../assets/banners/food-banner3.jpg')}
+              resizeMode="cover"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Amazing Food Place</Text>
+            <StarRating ratings={4} reviews={99} />
+            <Text style={styles.cardDetails}>
+              Some description for this amazing food place
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View style={styles.cardImgWrapper}>
+            <Image
+              source={require('../assets/banners/food-banner3.jpg')}
+              resizeMode="cover"
+              style={styles.cardImg}
+            />
+          </View>
+          <View style={styles.cardInfo}>
+            <Text style={styles.cardTitle}>Amazing Food Place</Text>
+            <StarRating ratings={4} reviews={99} />
+            <Text style={styles.cardDetails}>
+              Some description for this amazing food place
+            </Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
