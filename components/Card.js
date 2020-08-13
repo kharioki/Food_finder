2e88371abf0607dfb4267c8cf1ff8346ecb67437
kharioki <antolisah@gmail.com>
2020-08-13 03:40:29 +0300

    card component

---
+++ components/Card.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
+import StarRating from './StarRating';
+
+export default function Card() {
+  return (
+    <TouchableOpacity onPress={() => {}}>
+      <View style={styles.card}>
+        <View style={styles.cardImgWrapper}>
+          <Image
+            source={require('../assets/banners/food-banner3.jpg')}
+            resizeMode="cover"
+            style={styles.cardImg}
+          />
+        </View>
+        <View style={styles.cardInfo}>
+          <Text style={styles.cardTitle}>Amazing Food Place</Text>
+          <StarRating ratings={4} reviews={99} />
+          <Text style={styles.cardDetails}>
+            Some description for this amazing food place
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardsWrapper: {
+    marginTop: 20,
+    width: '90%',
+    alignSelf: 'center',
+  },
+  card: {
+    height: 100,
+    marginVertical: 10,
+    flexDirection: 'row',
+    shadowColor: '#999',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  cardImgWrapper: {
+    flex: 1,
+  },
+  cardImg: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    borderRadius: 8,
+    borderBottomRightRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  cardInfo: {
+    flex: 2,
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderBottomRightRadius: 8,
+    borderTopRightRadius: 8,
+    backgroundColor: '#fff',
+  },
+  cardTitle: {
+    fontWeight: 'bold',
+  },
+  cardDetails: {
+    fontSize: 12,
+    color: '#444',
+  },
+});
