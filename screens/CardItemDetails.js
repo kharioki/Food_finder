ef9c76951b2047432015a47bf00f916b0d7083b7
kharioki <antolisah@gmail.com>
2020-08-13 06:25:20 +0300

    added react native image header and updated triggering view

--- screens/CardItemDetails.js
+++ screens/CardItemDetails.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  Platform,
+  Dimensions,
+} from 'react-native';
+import HeaderImageScrollView, {
+  TriggeringView,
+} from 'react-native-image-header-scroll-view';
+
+const MIN_HEIGHT = Platform.OS === 'ios' ? 90 : 55;
+const MAX_HEIGHT = 350;
 
 export default function CardItemDetails({route}) {
   const itemData = route.params.itemData;
   return (
-    <View>
-      <Text>{itemData.title}</Text>
+    <View style={styles.container}>
+      <HeaderImageScrollView
+        maxHeight={MAX_HEIGHT}
+        minHeight={MIN_HEIGHT}
+        renderHeader={() => {
+          <Image source={itemData.image} style={{}} />;
+        }}>
+        <TriggeringView>
+          <View>
+            <Text style={styles.title}>Overview</Text>
+          </View>
+        </TriggeringView>
+      </HeaderImageScrollView>
     </View>
   );
 }
@@ -15,5 +39,76 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '90%',
     alignSelf: 'center',
+  },
+  image: {
+    height: MAX_HEIGHT,
+    width: Dimensions.get('window').width,
+    alignSelf: 'stretch',
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 20,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+  section: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    backgroundColor: 'white',
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  sectionContent: {
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  categories: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap',
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#FF6347',
+    borderRadius: 20,
+    margin: 10,
+    padding: 10,
+    paddingHorizontal: 15,
+  },
+  category: {
+    fontSize: 14,
+    color: '#fff',
+    marginLeft: 10,
+  },
+  titleContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageTitle: {
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 24,
+  },
+  navTitleView: {
+    height: MIN_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'ios' ? 40 : 5,
+    opacity: 0,
+  },
+  navTitle: {
+    color: 'white',
+    fontSize: 18,
+    backgroundColor: 'transparent',
+  },
+  sectionLarge: {
+    minHeight: 300,
   },
 });
